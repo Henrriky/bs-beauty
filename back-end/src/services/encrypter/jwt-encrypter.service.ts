b1dcf01c409ebd4f0bd5b26a3325492ceef957f0
Henrriky <tectutors.123@gmail.com>
2024-11-16 14:23:53 -0300

    feat: implement google auth identity provider and jwt encrypter

---
+++ back-end/src/services/encrypter/jwt-encrypter.service.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { type CustomerOrEmployee } from '../../types/customer-or-employee.type'
+import { type Encrypter } from '../protocols/encrypter.protocol'
+import { ENV } from '../../config/env'
+
+class JwtEncrypterService implements Encrypter {
+  async encrypt (params: CustomerOrEmployee & { userId: string }): Promise<{ accessToken: string }> {
+    const accessToken = jwt.sign(
+      {
+        sub: params.userId,
+        role: params.role,
+        email: params.email,
+        name: params.name,
+        registerCompleted: params.registerCompleted
+      },
+      ENV.JWT_SECRET,
+      {
+        expiresIn: ENV.JWT_EXPIRES_IN
+      }
+    )
+
+    return { accessToken }
+  }
+}
+
+export { JwtEncrypterService }
---
+++ back-end/src/services/identity-providers/google-oauth-identity-provider.service.ts
@@ -0,0 +1,28 @@
+import { ENV } from '../../config/env'
+import { oauth2Client } from '../../lib/google'
+import { type OAuthIdentityProvider } from '../protocols/oauth-identity-provider.protocol'
+
+class GoogleAuthIdentityProvider implements OAuthIdentityProvider {
+  async fetchEmailAndUserIdFromToken (token: string): Promise<{ userId: string, email: string }> {
+    const tokenInfo = await oauth2Client.getTokenInfo(token)
+    const { email, sub: userId } = tokenInfo
+
+    if ((email == null) || (userId == null)) {
+      throw new Error('Email or user id not exists on google token')
+    }
+
+    return { userId: 'asda', email: 'asdasd' }
+  }
+
+  generateRedirectUri () {
+    const authorizationUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: ENV.GOOGLE_SCOPES.split(','),
+      include_granted_scopes: true
+    })
+
+    return authorizationUrl
+  }
+}
+
+export { GoogleAuthIdentityProvider }
