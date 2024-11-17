0ad828a4a078dee52a4c0b25729b48ddede57348
Henrique Santiago <henrique14piressantiago@gmail.com>
2024-11-17 20:48:43 -0300

    chore: config initial settings with TailwindCSS

--- front-end/src/pages/LoginPage.tsx
+++ front-end/src/pages/LoginPage.tsx
@@ -1,35 +1,12 @@
-import './LoginPage.css'
-import personIcon from '../assets/personIcon.svg'
 import googleIcon from '../assets/google.svg'
-import lockIcon from '../assets/lock.svg'
+import './LoginPage.css'
 
 function LoginPage() {
     return (
         <>
             <form className='form' action="">
-                <div className='inputGroup'>
-                    <div className='inputItem'>
-                        <div>
-                            <img src={personIcon} className='icon'></img>
-                            <input type="text" placeholder='Nome de Usuário' />
-                        </div>
-                        <hr />
-                    </div>
-                    <div className='inputItem'>
-                        <div>
-                            <img src={lockIcon} className='icon'></img>
-                            <input type="text" placeholder='Senha' />
-                        </div>
-                        <hr />
-                    </div>
-                </div>
                 <div className='loginButtons'>
-                    <button className="mailLogin" type="submit">Login</button>
-                    <button className="googleLogin"><img src={googleIcon} alt="" />Logar com o Google</button>
-                </div>
-                <div className='auxOptions'>
-                    <a className='auxOptionsItem' href="">Esqueceu a senha</a>
-                    <a className='auxOptionsItem' href="">Entrar</a>
+                    <button className="flex justify-center items-center gap-2.5 bg-gray-300 p-2 rounded"><img src={googleIcon} alt="" />Logar com o Google</button>
                 </div>
             </form>
         </>
