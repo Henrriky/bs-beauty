0463c130a628f4e5c9d27c4f47166392e35d75d2
Henrriky <tectutors.123@gmail.com>
2024-11-19 00:26:29 -0300

    feat: define and implement exchangeCodeByToken on identity providers

--- back-end/src/services/identity-providers/google-oauth-identity-provider.service.ts
+++ back-end/src/services/identity-providers/google-oauth-identity-provider.service.ts
@@ -3,6 +3,7 @@ import { oauth2Client } from '../../lib/google'
 import { type OAuthIdentityProvider } from '../protocols/oauth-identity-provider.protocol'
 
 class GoogleAuthIdentityProvider implements OAuthIdentityProvider {
+  
   async fetchEmailAndUserIdFromToken (token: string): Promise<{ userId: string, email: string }> {
     const tokenInfo = await oauth2Client.getTokenInfo(token)
     const { email, sub: userId } = tokenInfo
@@ -12,6 +13,18 @@ class GoogleAuthIdentityProvider implements OAuthIdentityProvider {
     }
 
     return { userId: userId, email: email }
+  }
+  
+  async exchangeCodeByToken (code: string): Promise<{ accessToken: string }> {
+    
+  const { tokens } = await oauth2Client.getToken(code)
+  oauth2Client.setCredentials(tokens)
+
+  if (!tokens.access_token) {
+    throw new Error(`Error trying to exchange code by token: access_token property null or undefined ${tokens}`)
+  }
+
+  return { accessToken: tokens.access_token }
   }
 
   generateRedirectUri () {
--- back-end/src/services/protocols/oauth-identity-provider.protocol.ts
+++ back-end/src/services/protocols/oauth-identity-provider.protocol.ts
@@ -1,6 +1,7 @@
 interface OAuthIdentityProvider {
   fetchEmailAndUserIdFromToken: (token: string) => Promise<{ userId: string, email: string }>
   generateRedirectUri: () => string
+  exchangeCodeByToken: (code: string) => Promise<{ accessToken: string }>
 }
 
 export type { OAuthIdentityProvider }
