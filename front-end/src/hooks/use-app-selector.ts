9f3eeb377c3b861a2f5a3389caa5d101704e6229
Henrriky <tectutors.123@gmail.com>
2024-11-19 01:11:32 -0300

    feat: create hooks to use redux

---
+++ front-end/src/hooks/use-app-selector.ts
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux";
+import { store } from "../store";
+
+type RootState = ReturnType<typeof store.getState>;
+
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default useAppSelector;
