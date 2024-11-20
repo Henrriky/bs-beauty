932cab4697359237c953b906a3a22711d1f6c66b
Henrriky <tectutors.123@gmail.com>
2024-11-20 15:54:04 -0300

    feat: specify routes on router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,8 +2,16 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { ToastContainer } from 'react-toastify'
+
 import Layout from './layouts/Layout'
-import LoginPage from './pages/LoginPage'
+import Callback from './pages/callback'
+import PrivateRoute from './pages/private-route'
+
+import 'react-toastify/dist/ReactToastify.css'
+import Profile from './pages/profile'
+import Login from './pages/login'
+import CompleteRegister from './pages/complete-register'
 
 function App() {
   return (
@@ -12,11 +20,19 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route index element={<LoginPage />} />
-              <Route path="/callback" element={<Callback />} />
+              <Route index path="/login" element={<Login />} />
+              <Route path="/auth/google/callback" element={<Callback />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/profile" element={<Profile />} />
+                <Route
+                  path="/complete-register"
+                  element={<CompleteRegister />}
+                />
+              </Route>
             </Route>
           </Routes>
         </Router>
+        <ToastContainer />
       </Provider>
     </>
   )
