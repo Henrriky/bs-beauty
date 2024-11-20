94bfb8172c5e42e2185992584a2d7e62fc780340
Henrriky <tectutors.123@gmail.com>
2024-11-20 15:48:10 -0300

    fix: use jwt_decode instead jsonwebtoken

--- front-end/src/utils/decode-token.ts
+++ front-end/src/utils/decode-token.ts
@@ -1,9 +1,10 @@
 import { CustomerOrEmployee } from '../store/auth/types'
 import jwt from 'jsonwebtoken'
+import { jwtDecode } from 'jwt-decode'
 
 export interface TokenPayload extends jwt.JwtPayload, CustomerOrEmployee {}
 
 export const decodeUserToken = (token: string): TokenPayload => {
-  const decoded = jwt.decode(token) as TokenPayload
+  const decoded = jwtDecode<TokenPayload>(token)
   return decoded
 }
