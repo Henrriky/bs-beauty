9c1aeab0237e252c6eefa348a0d5b026a6311984
henrriky.dev <henrriky.dev@gmail.com>
2024-11-20 00:53:41 +0000

    feat: complement logic of callback page

--- front-end/src/pages/Callback.tsx
+++ front-end/src/pages/Callback.tsx
@@ -4,9 +4,9 @@ import loginBackgroundBottom from '../assets/login-background-bottom.svg'
 import loginBackgroundTop from '../assets/login-background-top.svg'
 import * as AuthAPI from '../api/auth-api'
 import { useNavigate } from 'react-router'
-import { useDispatch } from 'react-redux'
 import useAppDispatch from '../hooks/use-app-dispatch'
 import { setToken } from '../store/auth/auth-slice'
+import { TokenPayload, decodeUserToken } from '../utils/decode-token'
 
 
 interface AuthState {
@@ -14,18 +14,32 @@ interface AuthState {
   error: string | null;
 }
 
-function LoginPage() {
+function Callback() {
 
     const dispatchRedux = useAppDispatch()
     const [authState, setAuthState] = useState<AuthState>({ isLoading: true, error: null })
 
     const navigate = useNavigate();
 
-
     const extractAuthorizationCodeFromSearch = () => {
       const urlParams = new URLSearchParams(window.location.search)
       const code = urlParams.get('code')
       return code
+    }
+
+    const storeDecodedTokenOnAuthState = (decodedToken: TokenPayload, accessToken: string) => {
+      dispatchRedux(setToken({
+        user: {
+          role: decodedToken.role,
+          email: decodedToken.email,
+          name: decodedToken.name,
+          registerCompleted: decodedToken.registerCompleted
+        },
+        token: {
+          accessToken,
+          expiresAt: decodedToken.exp!
+        }
+      }))
     }
 
     useEffect(() => {
@@ -38,19 +52,11 @@ function LoginPage() {
       AuthAPI.exchangeCodeForToken(code)
         .then(({ accessToken }) => {
 
-          //TODO: Armazenar o accessToken na Slice do usuario
-          dispatchRedux(setToken({
-            user: {
-              role: "MANAGER",
-              
-            },
-            token: {
-              accessToken,
-              expiresAt: 1
-            }
-          }))
-
-
+          const decodedToken = decodeUserToken(accessToken)
+          storeDecodedTokenOnAuthState(decodedToken, accessToken)
+          if (decodedToken.registerCompleted) {
+            
+          }
           // localStorage.setItem('token', accessToken)
           // navigate('/home')
         })
@@ -67,39 +73,12 @@ function LoginPage() {
             isLoading: false
           });
         })
-
-
     }, [])
-
-    async function handleButtonClick () {
-        try {
-            const { authorizationUrl }  = await AuthAPI.fetchGoogleRedirectUri()
-            window.location.href = authorizationUrl
-        } catch (error) {
-            console.error(error)
-            setError('Erro ao buscar a URL de Autenticação')
-        }
-    }
-
     return (
         <>
-            <div className='flex justify-center items-center h-screen flex-col relative'>
-                <img src={loginBackgroundTop} alt="Ícone do Google" className='absolute right-0 top-0'/>
-                <div>
-                    { error && <p className='text-red-400'>{error}</p>}
-                    <button 
-                        onClick={handleButtonClick}
-                        className="flex justify-center items-center gap-2.5 bg-[#DBDBDB] p-2 rounded w-[320px] text-[#1E1E1E] text-sm font-medium rounded-tl-3xl rounded-tr-sm rounded-br-3xl rounded-bl-sm"
-                    >
-                        <img src={googleIcon} alt="Ícone do Google" />
-                        Logar com o Google
-                    </button>
-                </div>
-
-                <img src={loginBackgroundBottom} alt="Ícone do Google" className='absolute left-0 bottom-0 max-h- object-cover w-full'/>
-            </div>
+          { authState.error && <p className='text-red-400'>{authState.error}</p>}
         </>
     )
 }
 
-export default LoginPage
+export default Callback
--- front-end/src/utils/decode-token.ts
+++ front-end/src/utils/decode-token.ts
@@ -2,7 +2,7 @@
 import { CustomerOrEmployee } from '../store/auth/types';
 import jwt from 'jsonwebtoken';
 
-interface TokenPayload extends jwt.JwtPayload, CustomerOrEmployee {}
+export interface TokenPayload extends jwt.JwtPayload, CustomerOrEmployee {}
 
 export const decodeUserToken = (token: string): TokenPayload => {
   const decoded = jwt.decode(token) as TokenPayload
