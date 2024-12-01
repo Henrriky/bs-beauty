0b039eb1629849af4536b62cb775c2ba7679f93c
Henrriky <tectutors.123@gmail.com>
2024-12-01 00:57:57 -0300

    feat: create button variations, as solid or outline and border solid or dashed, furthermore now allow also className override

--- front-end/src/components/inputs/Input.tsx
+++ front-end/src/components/inputs/Input.tsx
@@ -1,12 +1,17 @@
 import { HTMLInputTypeAttribute, InputHTMLAttributes } from 'react'
 import { UseFormRegisterReturn } from 'react-hook-form'
+import { ErrorMessage } from '../feedback/ErrorMessage'
+import clsx from 'clsx'
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string
+  label?: string
   id: string
   type: HTMLInputTypeAttribute
   error?: string | null
   registration?: UseFormRegisterReturn
+  variant?: 'solid' | 'outline'
+  wrapperClassName?: string | undefined
+  inputClassName?: string | undefined
 }
 
 export function Input({
@@ -15,22 +20,39 @@ export function Input({
   type,
   error,
   registration,
+  wrapperClassName,
+  inputClassName,
+  variant = 'outline',
   ...htmlProps
 }: InputProps) {
+  const borderColor = error
+    ? 'border-[#CC3636]'
+    : variant === 'outline'
+      ? 'border-[#3B3B3B]'
+      : 'border-white'
+
   return (
-    <div className="flex gap-3 flex-col items-start">
-      <label className="text-sm text-[#D9D9D9]" htmlFor={id}>
-        {label}
-      </label>
+    <div className={clsx('flex gap-3 flex-col items-start', wrapperClassName)}>
+      {label && (
+        <label className="text-sm text-[#D9D9D9]" htmlFor={id}>
+          {label}
+        </label>
+      )}
       <input
         {...registration}
         {...htmlProps}
         id={id}
         type={type}
-        className={`text-sm text-[#A5A5A5] bg-transparent focus:outline-none cursor-text w-full border-solid border-b-2 
-          ${error ? 'border-[#CC3636]' : 'border-[#3B3B3B]'} pb-2 focus-within:border-[#B19B86] transition-colors duration-300`}
+        className={clsx(
+          variant === 'outline'
+            ? `text-sm text-[#A5A5A5] bg-transparent focus:outline-none cursor-text w-full border-solid border-b-2 ${borderColor} pb-2 focus-within:border-[#B19B86] transition-colors duration-300`
+            : variant === 'solid'
+              ? `w-full text-sm text-[#A5A5A5] bg-[#1a1a1a] focus:outline-none cursor-text ${borderColor} border-[1px] border-opacity-10 rounded-2xl px-2 py-[10px] focus-within:border-[#B19B86] transition-colors duration-300 `
+              : '',
+          inputClassName,
+        )}
       />
-      {error && <p className="text-[#CC3636] text-sm font-medium">{error}</p>}
+      {error && <ErrorMessage message={error} />}
     </div>
   )
 }
