930eb7b2857aea670348dd557a87cccad5020ce6
Henrriky <tectutors.123@gmail.com>
2024-12-01 00:58:16 -0300

    feat: customize toast style

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { ToastContainer } from 'react-toastify'
+import { Flip, ToastContainer } from 'react-toastify'
 
 import BSBeautyRouter from './routes'
 
@@ -11,7 +11,21 @@ function App() {
     <>
       <Provider store={store}>
         <BSBeautyRouter />
-        <ToastContainer />
+        <ToastContainer
+          transition={Flip}
+          position="top-right"
+          toastClassName={
+            'bg-[#595149] text-[#D9D9D9] font-sans text-bold rounded-full font-medium pt-2'
+          }
+          closeButton={({ closeToast }) => (
+            <button
+              className="text-[#B19B86] text-lg focus:outline-none hover:text-[#D9D9D9] transition duration-300 ease-in-out right-5 top-1 absolute"
+              onClick={closeToast}
+            >
+              ✖
+            </button>
+          )}
+        />
       </Provider>
     </>
   )
