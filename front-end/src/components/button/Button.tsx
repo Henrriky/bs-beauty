d0031a5950ae5b992024e36c84d80e6036d4473f
Henrriky <tectutors.123@gmail.com>
2024-12-01 00:57:40 -0300

    feat: create button variations, as solid or outline and border solid or dashed, furthermore now allow also className override

--- front-end/src/components/button/Button.tsx
+++ front-end/src/components/button/Button.tsx
@@ -1,16 +1,41 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react'
+import clsx from 'clsx'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string | ReactNode
+  variant?: 'outline' | 'solid'
+  outlineVariantBorderStyle?: 'solid' | 'dashed'
+  className?: string
 }
 
-export function Button(props: ButtonProps) {
+export function Button({
+  label,
+  variant = 'solid',
+  outlineVariantBorderStyle = 'solid',
+  className,
+  ...props
+}: ButtonProps) {
+  // This ugly conditional is being applied because of a behavior in tailwind that does not dynamically identify classes -> https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
+  const outlineBorderStyle =
+    outlineVariantBorderStyle === 'solid'
+      ? 'border-solid'
+      : outlineVariantBorderStyle === 'dashed'
+        ? 'border-dashed'
+        : ''
+
   return (
     <button
       {...props}
-      className="bg-[#595149] text-[#D9D9D9] transition-all duration-300 ease-in-out items-center justify-center w-full text-sm py-3 rounded-3xl active:bg-[#3A3027] cursor"
+      className={clsx(
+        variant === 'outline'
+          ? `bg-none text-[#bebebe] transition-all duration-300 ease-in-out items-center justify-center w-full text-sm py-3 rounded-3xl active:bg-[#3A3027] cursor border-[#B19B86] ${outlineBorderStyle} border-[1.5px] hover:bg-[#B19B86] hover:bg-opacity-10 cursor-pointer`
+          : variant === 'solid'
+            ? 'bg-[#595149] text-[#D9D9D9] transition-all duration-300 ease-in-out items-center justify-center w-full text-sm py-3 rounded-3xl active:bg-[#3A3027] cursor hover:bg-[#3A3027]'
+            : '',
+        className,
+      )}
     >
-      {props.label}
+      {label}
     </button>
   )
 }
