03f88b9c5a2e13af70954d73590078246710913c
Henrriky <tectutors.123@gmail.com>
2024-12-01 01:04:52 -0300

    feat: navigate to register completed in case of error and home if person has already registered

--- front-end/src/pages/complete-register/index.tsx
+++ front-end/src/pages/complete-register/index.tsx
@@ -1,13 +1,18 @@
+import { toast } from 'react-toastify'
+
 import Title from '../../components/texts/Title'
-import { Role } from '../../store/auth/types'
-import useAppSelector from '../../hooks/use-app-selector'
 import CustomerInputContainer from './components/CustomerInputContainer'
 import EmployeeInputContainer from './components/EmployeeInputContainer'
-import { OnSubmitEmployeeOrCustomerForm } from './components/types'
-import { authAPI } from '../../store/auth/auth-api'
-import { toast } from 'react-toastify'
+
 import useAppDispatch from '../../hooks/use-app-dispatch'
+import useAppSelector from '../../hooks/use-app-selector'
+import { authAPI } from '../../store/auth/auth-api'
 import { setRegisterCompleted } from '../../store/auth/auth-slice'
+
+import { Role } from '../../store/auth/types'
+import { OnSubmitEmployeeOrCustomerForm } from './components/types'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 const rolesToInputContainers = {
   [Role.CUSTOMER]: CustomerInputContainer,
@@ -16,32 +21,43 @@ const rolesToInputContainers = {
 }
 
 function CompleteRegister() {
+  const navigate = useNavigate()
+  const dispatchRedux = useAppDispatch()
+  const user = useAppSelector((state) => state.auth.user!)
   const [completeRegister, { isLoading }] =
     authAPI.useCompleteRegisterMutation()
 
-  const dispatchRedux = useAppDispatch()
-  const userRole = useAppSelector((state) => state.auth.user!.role)
-  const InputContainer = rolesToInputContainers[userRole]
+  const InputContainer = rolesToInputContainers[user.role]
 
   // REFACTOR TODO: Extract bellow function and useEffect to an customHook
   const handleSubmit: OnSubmitEmployeeOrCustomerForm = async (data) => {
     await completeRegister(data)
       .unwrap()
       .then(() => {
-        toast.success('Parabéns, seu cadastro foi concluído com sucesso!')
         dispatchRedux(
           // TODO: Improve this refreshing token by complete register route
           setRegisterCompleted(),
         )
+        navigate('/register-completed')
       })
       .catch((error: unknown) => {
-        console.log('Error trying to complete register', error)
+        console.error('Error trying to complete register', error)
         toast.error('Ocorreu um erro ao completar o seu cadastro.')
       })
   }
 
+  useEffect(() => {
+    if (user.registerCompleted) {
+      toast.info(
+        'Você já completou seu registro, vamos te enviar para a tela inicial',
+      )
+      navigate('/home')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div className="flex justify-center items-center flex-col h-full gap-12">
+    <div className="flex justify-center items-center flex-col h-full gap-12 animate-fadeIn">
       <Title align="center">Quase lá, finalize seu cadastro</Title>
       <InputContainer handleSubmit={handleSubmit} isLoading={isLoading} />
     </div>
