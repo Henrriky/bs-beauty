97aa1c52c102d082a1864035f7e2cfd038fbdf10
Henrriky <tectutors.123@gmail.com>
2024-12-08 18:48:16 -0300

    refactor: use social media input component on employee profile

--- front-end/src/pages/profile/components/EmployeeProfile.tsx
+++ front-end/src/pages/profile/components/EmployeeProfile.tsx
@@ -10,9 +10,6 @@ import { Button } from '../../../components/button/Button'
 import { userAPI } from '../../../store/user/user-api'
 import { EmployeeUpdateProfileFormData } from '../types'
 import { EmployeeSchemas } from '../../../utils/validation/zod-schemas/employee.zod-schemas.validation.utils'
-import { ErrorMessage } from '../../../components/feedback/ErrorMessage'
-import { PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
-import { getErrorMessageFromErrorsAttr } from '../../../utils/react-hook-form/get-error-message-from-errors-attr'
 import SocialMediaContainerInput from '../../../components/inputs/social-media-input/SocialMediaContainerInput'
 
 interface EmployeeProfileProps {
@@ -113,85 +110,6 @@ function EmployeeProfile({ userInfo }: EmployeeProfileProps) {
         errors={errors}
         appendNewSocialMedia={appendNewSocialMedia}
       />
-      {/* <div className="flex gap-3 flex-col items-start">
-        <label className="text-sm text-[#D9D9D9]" htmlFor="socialMedia">
-          Redes sociais
-        </label>
-        <Button
-          type="button"
-          variant="outline"
-          disabled={socialMediaFields.length >= 5}
-          outlineVariantBorderStyle="dashed"
-          onClick={() => appendNewSocialMedia({ name: '', url: '' })}
-          label={
-            <div className="flex justify-center items-center gap-1">
-              <PlusIcon className="size-5" />
-              <p className="text-sm">
-                {socialMediaFields.length >= 5
-                  ? 'Limite atingido'
-                  : 'Clique para uma nova'}
-              </p>
-            </div>
-          }
-        />
-        {errors?.socialMedia?.message && (
-          <ErrorMessage message={errors.socialMedia.message} />
-        )}
-        <div className="pl-4 w-full max-h-80 overflow-y-scroll scroll">
-          {socialMediaFields.map((field, index) => {
-            const socialMediaNameField =
-              `socialMedia.${index}.name` as `socialMedia.${number}.name`
-            const socialMediaUrlField =
-              `socialMedia.${index}.url` as `socialMedia.${number}.url`
-
-            return (
-              <div
-                key={field.id}
-                className="flex mt-4 gap-2 justify-start items-center"
-              >
-                <div className="flex gap-2 w-full">
-                  <Input
-                    registration={{ ...register(socialMediaNameField) }}
-                    id={socialMediaNameField}
-                    type="text"
-                    placeholder="Nome da rede social"
-                    variant="solid"
-                    error={getErrorMessageFromErrorsAttr(
-                      errors,
-                      socialMediaNameField,
-                    )}
-                    wrapperClassName="w-full"
-                  />
-                  <Input
-                    registration={{ ...register(socialMediaUrlField) }}
-                    id={socialMediaUrlField}
-                    type="text"
-                    placeholder="Link da rede social"
-                    variant="solid"
-                    error={getErrorMessageFromErrorsAttr(
-                      errors,
-                      socialMediaUrlField,
-                    )}
-                    wrapperClassName="w-full"
-                  />
-                </div>
-                <Button
-                  type="button"
-                  variant="outline"
-                  onClick={() => removeSocialMedia(index)}
-                  label={
-                    <div className="flex justify-center items-center gap-1">
-                      <TrashIcon className="size-6" />
-                    </div>
-                  }
-                  disabled={isLoading}
-                  className="max-w-10 border-none hover:text-[#B19B86] rounded-none hover:bg-opacity-0 hover:bg-transparent"
-                />
-              </div>
-            )
-          })}
-        </div>
-      </div> */}
       <Button
         type="submit"
         label={
