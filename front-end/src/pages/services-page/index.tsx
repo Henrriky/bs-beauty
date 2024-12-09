8c319291e1d065f732abff90c8aebcca650e392f
Henrriky <tectutors.123@gmail.com>
2024-12-08 21:03:49 -0300

    refactor: improve max width device

--- front-end/src/pages/services-page/index.tsx
+++ front-end/src/pages/services-page/index.tsx
@@ -1,14 +1,11 @@
-import SideBar from "../../components/sidebar/SideBar"
+// TODO: Test page. Should be edited or deleted later on.
 
-//TODO: Test page. Should be edited or deleted later on.
-
-function ServicesPage () {
-    return (
-        <>
-            <SideBar />
-            <h1 className="text-primary-300 w-40">Bem vindo(a) ao seu perfil</h1>
-        </>
-    )
+function ServicesPage() {
+  return (
+    <>
+      <h1 className="text-primary-300 w-40">Bem vindo(a) ao seu perfil</h1>
+    </>
+  )
 }
 
 export default ServicesPage
