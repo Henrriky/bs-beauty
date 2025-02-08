c82a640b7c2abc69e50df6c7aba384fc214016f1
Henrriky <tectutors.123@gmail.com>
2025-02-08 19:26:33 -0300

    stash test

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -6,4 +6,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: './',
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
 })
