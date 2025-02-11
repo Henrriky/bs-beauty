530d36c52eaa21fa9fb80369152b805591972406
santiagohenrique <henrique11santiagopires@hotmail.com>
2025-02-10 22:08:25 -0300

    test: add missing unit tests for controllers

---
+++ back-end/tests/unit/controllers/auth/fetch-user-info.controller.spec.ts
@@ -0,0 +1,101 @@
+import { StatusCodes } from 'http-status-codes'
+import { FetchUserInfoController } from '../../../../src/controllers/auth/fetch-user-info.controller'
+import { makeFetchUserInfoUseCase } from '../../../../src/factory/auth/make-fetch-user-info.use-case.factory.ts'
+import { InvalidRoleUseCaseError } from '../../../../src/services/use-cases/errors/invalid-role-use-case-error'
+import { NotFoundUseCaseError } from '../../../../src/services/use-cases/errors/not-found-error'
+import { z } from 'zod'
+
+vi.mock('../../../../src/factory/auth/make-fetch-user-info.use-case.factory.ts')
+
+describe('FetchUserInfoController', () => {
+  let req: any
+  let res: any
+  let useCaseMock: any
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+
+    req = {
+      user: {
+        email: 'user@example.com',
+        role: 'user'
+      }
+    }
+
+    res = {
+      status: vi.fn().mockReturnThis(),
+      send: vi.fn(),
+      json: vi.fn()
+    }
+
+    useCaseMock = {
+      execute: vi.fn()
+    }
+
+    vi.mocked(makeFetchUserInfoUseCase).mockImplementation(() => useCaseMock)
+  })
+
+  it('should be defined', () => {
+    expect(FetchUserInfoController).toBeDefined()
+  })
+
+  describe('handle', () => {
+    it('should return 200 and the user info if the use case succeeds', async () => {
+      // arrange
+      const mockUser = { name: 'John Doe', email: 'user@example.com' }
+      useCaseMock.execute.mockResolvedValueOnce({ user: mockUser })
+
+      // act
+      await FetchUserInfoController.handle(req, res)
+
+      // assert
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
+      expect(res.send).toHaveBeenCalledWith({ user: mockUser })
+      expect(useCaseMock.execute).toHaveBeenCalledWith({
+        email: 'user@example.com',
+        role: 'user'
+      })
+    })
+
+    it('should return 400 if the role is invalid', async () => {
+      // arrange
+      const errorMessage = 'Invalid role'
+      useCaseMock.execute.mockRejectedValueOnce(new InvalidRoleUseCaseError(errorMessage))
+
+      // act
+      await FetchUserInfoController.handle(req, res)
+
+      // assert
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST)
+      expect(res.send).toHaveBeenCalledWith({ message: errorMessage })
+    })
+
+    it('should return 404 if the user is not found', async () => {
+      // arrange
+      const errorMessage = 'User not found'
+      useCaseMock.execute.mockRejectedValueOnce(new NotFoundUseCaseError(errorMessage))
+
+      // act
+      await FetchUserInfoController.handle(req, res)
+
+      // assert
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.NOT_FOUND)
+      expect(res.send).toHaveBeenCalledWith({ message: errorMessage })
+    })
+
+    it('should return 500 if the use case throws an unexpected error', async () => {
+      // arrange
+      useCaseMock.execute.mockRejectedValueOnce(new Error('Unexpected error'))
+
+      // act
+      await FetchUserInfoController.handle(req, res)
+
+      // assert
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.INTERNAL_SERVER_ERROR)
+      expect(res.send).toHaveBeenCalledWith({
+        message: 'Error trying to fetch user info, please check back-end logs...'
+      })
+    })
+
+  })
+})
---
+++ back-end/tests/unit/controllers/offers.controller.spec.ts
@@ -0,0 +1,429 @@
+import { Offer } from "@prisma/client";
+import { OffersController } from "../../../src/controllers/offers.controller";
+import { makeOffersUseCaseFactory } from "../../../src/factory/make-offers-use-case.factory";
+import { Decimal } from "@prisma/client/runtime/library";
+
+vi.mock('../../../src/factory/make-offers-use-case.factory.ts');
+
+describe('OffersController', () => {
+
+  let req: any;
+  let res: any;
+  let next: any;
+  let useCaseMock: any;
+
+  beforeEach(() => {
+
+    vi.clearAllMocks();
+
+    req = {
+      user: { userId: 'user-123' },
+      body: {},
+      params: {},
+      query: {},
+    };
+
+
+
+    res = {
+      status: vi.fn().mockReturnThis(),
+      send: vi.fn(),
+    };
+
+    next = vi.fn();
+
+    useCaseMock = {
+      executeFindAll: vi.fn(),
+      executeFindById: vi.fn(),
+      executeFindByServiceId: vi.fn(),
+      executeFindByEmployeeId: vi.fn(),
+      executeCreate: vi.fn(),
+      executeUpdate: vi.fn(),
+      executeDelete: vi.fn(),
+      executeFetchAvailableSchedulingToOfferByDay: vi.fn(),
+    };
+
+    vi.mocked(makeOffersUseCaseFactory).mockReturnValue(useCaseMock);
+    vi.setSystemTime(new Date('2025-01-01T09:00:00'));
+
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('should be defined', () => {
+    expect(OffersController).toBeDefined();
+  });
+
+  describe('handleFindAll', () => {
+    it('should find all offers', async () => {
+
+      const mockOffers = [
+        {
+          id: 'random-uuid',
+          serviceId: 'random-uuid-service',
+          employeeId: 'random-uuid-employee',
+          estimatedTime: 60,
+          price: new Decimal(100.0),
+          isOffering: true,
+          createdAt: new Date('2025-01-01T09:00:00'),
+          updatedAt: new Date('2025-01-01T09:00:00'),
+        }
+      ] as Offer[];
+
+      // arrange
+      useCaseMock.executeFindAll.mockResolvedValueOnce({ offers: mockOffers });
+
+      // act
+      await OffersController.handleFindAll(req, res, next);
+
+      // assert
+      expect(res.send).toHaveBeenCalledWith({ offers: mockOffers });
+      expect(useCaseMock.executeFindAll).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeFindAll fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeFindAll.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleFindAll(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeFindAll).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('handleFindById', () => {
+    it('should find an offer by id', async () => {
+
+      // arrange
+      const mockOffer = {
+        id: 'random-uuid',
+        serviceId: 'random-uuid-service',
+        employeeId: 'random-uuid-employee',
+        estimatedTime: 60,
+        price: new Decimal(100.0),
+        isOffering: true,
+        createdAt: new Date('2025-01-01T09:00:00'),
+        updatedAt: new Date('2025-01-01T09:00:00'),
+      } as Offer;
+
+      req.params.id = 'random-uuid';
+      useCaseMock.executeFindById.mockResolvedValueOnce(mockOffer);
+
+      // act
+      await OffersController.handleFindById(req, res, next);
+
+      // assert
+      expect(req.params.id).toBe('random-uuid');
+      expect(res.send).toHaveBeenCalledWith(mockOffer);
+      expect(useCaseMock.executeFindById).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeFindById fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeFindById.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleFindById(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeFindById).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('handleFindByServiceId', () => {
+    it('should find an offer by service id', async () => {
+
+      // arrange
+      const mockOffer = {
+        id: 'random-uuid',
+        serviceId: 'random-uuid-service',
+        employeeId: 'random-uuid-employee',
+        estimatedTime: 60,
+        price: new Decimal(100.0),
+        isOffering: true,
+        createdAt: new Date('2025-01-01T09:00:00'),
+        updatedAt: new Date('2025-01-01T09:00:00'),
+      } as Offer;
+
+      req.params.serviceId = mockOffer.serviceId;
+      useCaseMock.executeFindByServiceId.mockResolvedValueOnce(mockOffer);
+
+      // act
+      await OffersController.handleFindByServiceId(req, res, next);
+
+      // assert
+      expect(req.params.serviceId).toBe(mockOffer.serviceId);
+      expect(res.send).toHaveBeenCalledWith(mockOffer);
+      expect(useCaseMock.executeFindByServiceId).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+
+    });
+
+    it('should call next with an error if executeFindByServiceId fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeFindByServiceId.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleFindByServiceId(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeFindByServiceId).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+
+  });
+
+  describe('handleFindByEmployeeId', () => {
+    it('should find offers by employee id', async () => {
+
+      // arrange
+      const mockOffers = [
+        {
+          id: 'random-uuid',
+          serviceId: 'random-uuid-service',
+          employeeId: 'random-uuid-employee',
+          estimatedTime: 60,
+          price: new Decimal(100.0),
+          isOffering: true,
+          createdAt: new Date('2025-01-01T09:00:00'),
+          updatedAt: new Date('2025-01-01T09:00:00'),
+        }
+      ] as Offer[];
+
+      req.params.employeeId = mockOffers[0].employeeId;
+      useCaseMock.executeFindByEmployeeId.mockResolvedValueOnce({ offers: mockOffers });
+
+      // act
+      await OffersController.handleFindByEmployeeId(req, res, next);
+
+      // assert
+      expect(req.params.employeeId).toBe(mockOffers[0].employeeId);
+      expect(res.send).toHaveBeenCalledWith({ offers: mockOffers });
+      expect(useCaseMock.executeFindByEmployeeId).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeFindByEmployeeId fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeFindByEmployeeId.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleFindByEmployeeId(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeFindByEmployeeId).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+
+  });
+
+  describe('handleCreate', () => {
+    it('should create an offer', async () => {
+
+      // arrange
+      const mockOffer = {
+        serviceId: 'random-uuid-service',
+        employeeId: 'random-uuid-employee',
+        estimatedTime: 60,
+        price: new Decimal(100.0),
+        isOffering: true,
+        createdAt: new Date('2025-01-01T09:00:00'),
+        updatedAt: new Date('2025-01-01T09:00:00'),
+      } as Offer;
+
+      req.body = mockOffer;
+      useCaseMock.executeCreate.mockResolvedValueOnce(mockOffer);
+
+      // act
+      await OffersController.handleCreate(req, res, next);
+
+      // assert
+      expect(req.body).toEqual(mockOffer);
+      expect(res.send).toHaveBeenCalledWith(mockOffer);
+      expect(useCaseMock.executeCreate).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeCreate fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeCreate.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleCreate(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeCreate).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('handleUpdate', () => {
+    it('should update an offer', async () => {
+
+      // arrange
+      const mockOffer = {
+        id: 'random-uuid',
+        serviceId: 'random-uuid-service',
+        employeeId: 'random-uuid-employee',
+        estimatedTime: 60,
+        price: new Decimal(100.0),
+        isOffering: true,
+        createdAt: new Date('2025-01-01T09:00:00'),
+        updatedAt: new Date('2025-01-01T09:00:00'),
+      } as Offer;
+
+      req.params.id = mockOffer.id;
+      req.body = mockOffer;
+      useCaseMock.executeUpdate.mockResolvedValueOnce(mockOffer);
+
+      // act
+      await OffersController.handleUpdate(req, res, next);
+
+      // assert
+      expect(req.params.id).toBe(mockOffer.id);
+      expect(req.body).toEqual(mockOffer);
+      expect(res.send).toHaveBeenCalledWith(mockOffer);
+      expect(useCaseMock.executeUpdate).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeUpdate fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeUpdate.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleUpdate(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeUpdate).toHaveBeenCalledTimes(1);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('handleDelete', () => {
+    it('should delete an offer', async () => {
+
+      // arrange
+      const mockOffer = {
+        id: 'random-uuid',
+        serviceId: 'random-uuid-service',
+        employeeId: 'random-uuid-employee',
+        estimatedTime: 60,
+        price: new Decimal(100.0),
+        isOffering: true,
+        createdAt: new Date('2025-01-01T09:00:00'),
+        updatedAt: new Date('2025-01-01T09:00:00'),
+      } as Offer;
+
+      req.params.id = mockOffer.id;
+      useCaseMock.executeDelete.mockResolvedValueOnce(mockOffer);
+
+      // act
+      await OffersController.handleDelete(req, res, next);
+
+      // assert
+      expect(req.params.id).toBe(mockOffer.id);
+      expect(res.send).toHaveBeenCalledWith(mockOffer);
+      expect(useCaseMock.executeDelete).toHaveBeenCalledTimes(1);
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if executeDelete fails', async () => {
+      // arrange
+      const error = new Error('Database connection failed');
+      useCaseMock.executeDelete.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleDelete(req, res, next);
+
+      // assert
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.send).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('handleFetchAvailableSchedulingToOfferByDay', () => {
+    it('should fetch available scheduling for an offer by day', async () => {
+      // arrange
+      const serviceOfferingId = 'random-service-id';
+      const dayToFetchAvailableSchedulling = '2025-02-10T00:00:00.000Z';
+      const availableSchedulling = [
+        { startTimestamp: '10:00 AM', endTimestamp: '10:00 AM', isBusy: false },
+        { startTimestamp: '12:00 AM', endTimestamp: '15:00 AM', isBusy: true },
+        { startTimestamp: '12:00 AM', endTimestamp: '15:00 AM', isBusy: true },
+        { startTimestamp: '11:00 AM', endTimestamp: '17:00 AM', isBusy: true },
+      ];
+
+      req.params.id = serviceOfferingId;
+      req.query.dayToFetchAvailableSchedulling = dayToFetchAvailableSchedulling;
+
+      useCaseMock.executeFetchAvailableSchedulingToOfferByDay.mockResolvedValueOnce({
+        availableSchedulling
+      });
+
+      // act
+      await OffersController.handleFetchAvailableSchedulingToOfferByDay(req, res, next);
+
+      // assert
+      expect(useCaseMock.executeFetchAvailableSchedulingToOfferByDay).toHaveBeenCalledWith(
+        serviceOfferingId,
+        new Date(dayToFetchAvailableSchedulling)
+      );
+      expect(res.send).toHaveBeenCalledWith({ availableSchedulling });
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    it('should call next with an error if use case fails', async () => {
+      // arrange
+      const error = new Error('Something went wrong');
+
+      req.params.id = 'random-service-id';
+      req.query.dayToFetchAvailableSchedulling = '2025-02-10T00:00:00.000Z';
+
+      useCaseMock.executeFetchAvailableSchedulingToOfferByDay.mockRejectedValueOnce(error);
+
+      // act
+      await OffersController.handleFetchAvailableSchedulingToOfferByDay(req, res, next);
+
+      // assert
+      expect(next).toHaveBeenCalledWith(error);
+      expect(res.send).not.toHaveBeenCalled();
+    });
+  });
+
+
+});
