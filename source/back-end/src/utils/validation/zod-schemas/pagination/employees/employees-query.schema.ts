8d7d1f965a5cca0ddf24031522d3d5bf0917a323
santiagohenrique <henrique11santiagopires@hotmail.com>
2025-06-07 22:50:15 -0300

    fix: add missing zod validation to verify string limit

--- source/back-end/src/utils/validation/zod-schemas/pagination/employees/employees-query.schema.ts
+++ source/back-end/src/utils/validation/zod-schemas/pagination/employees/employees-query.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 import { basePaginationSchema } from '../pagination.schema'
 
 export const employeeQuerySchema = basePaginationSchema.extend({
-  name: z.string().optional(),
-  email: z.string().optional(),
+  name: z.string().max(191).optional(),
+  email: z.string().max(191).optional(),
 })
 
 export type EmployeeQuerySchema = z.infer<typeof employeeQuerySchema>
