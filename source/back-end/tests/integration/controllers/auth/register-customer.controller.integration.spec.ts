f3502ae0aee3fb27bf2a24841a02ae9be203b38b
santiagohenrique <henrique11santiagopires@hotmail.com>
2025-08-31 15:14:22 -0300

    test: add sample auth integration tests

---
+++ source/back-end/tests/integration/controllers/auth/register-customer.controller.integration.spec.ts
@@ -0,0 +1,17 @@
+describe('RegisterCustomerController - POST /api/auth/register', () => {
+  it('should register a new customer successfully', async () => {
+    // arrange
+
+    // act
+
+    // assert
+  })
+
+  it('should throw a bad request exception when invalid inputs', async () => {
+    // arrange
+
+    // act
+
+    // assert
+  })
+})
--- source/back-end/tests/integration/utils/auth.ts
+++ source/back-end/tests/integration/utils/auth.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken';
+import { TokenPayload } from '../../../src/middlewares/auth/verify-jwt-token.middleware';
 
 export function getManagerToken(secret = process.env.JWT_SECRET as string) {
-  if (!secret) throw new Error('JWT_SECRET ausente nos testes.');
+  if (!secret) throw new Error('JWT_SECRET is not set');
   return jwt.sign({ sub: 'test-user-id', userType: 'MANAGER' }, secret, { expiresIn: '1h' });
+}
+
+export function signToken(payload: TokenPayload, secret = process.env.JWT_SECRET as string) {
+  if (!secret) throw new Error('JWT_SECRET ausente nos testes.')
+  return jwt.sign(payload, secret, { expiresIn: '1h' })
 }
