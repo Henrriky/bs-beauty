9c5504581a6635c85bee7293d7bd9acb275401b0
Alyson Fumagalli <alysoncesarfumagalli@gmail.com>
2025-09-08 04:32:54 -0300

    feat: model salon setting and create seed file

---
+++ source/back-end/prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.salonSettings.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: 'BS Beauty Academy',
+    },
+  })
+}
+
+main()
+  .then(() => console.log('✅ Settings seeded'))
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect())
