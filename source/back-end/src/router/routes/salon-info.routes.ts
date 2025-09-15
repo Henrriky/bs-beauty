e02c471a3063ea346e8b05c253fbc104b6f12af8
Alyson Fumagalli <alysoncesarfumagalli@gmail.com>
2025-09-15 01:09:56 -0300

    feat: add validation for salon info in back-end

--- source/back-end/src/router/routes/salon-info.routes.ts
+++ source/back-end/src/router/routes/salon-info.routes.ts
@@ -1,10 +1,11 @@
 import { SalonInfoController } from '@/controllers/salon-info.controller'
+import { validateUpdateSalonInfo } from '@/middlewares/data-validation/salon-info/update-salon-info.validation.middleware'
 import { routeAuthMiddleware } from '@/middlewares/route-auth.middleware'
 import { Router } from 'express'
 
 const salonInfoRoutes = Router()
 
 salonInfoRoutes.get('/:id', routeAuthMiddleware(['MANAGER', 'PROFESSIONAL', 'CUSTOMER']), SalonInfoController.handleFetchInfo)
-salonInfoRoutes.put('/:id', routeAuthMiddleware(['MANAGER']), SalonInfoController.handleUpdateInfo)
+salonInfoRoutes.put('/:id', routeAuthMiddleware(['MANAGER']), validateUpdateSalonInfo, SalonInfoController.handleUpdateInfo)
 
 export { salonInfoRoutes }
