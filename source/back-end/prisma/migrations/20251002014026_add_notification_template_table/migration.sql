c516944b5fd167c268337778f924df3cf5d74504
santiagohenrique <henrique11santiagopires@hotmail.com>
2025-10-02 23:02:55 -0300

    feat: add notification template table

---
+++ source/back-end/prisma/migrations/20251002014026_add_notification_template_table/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE `notification_template` (
+    `id` VARCHAR(191) NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `body` TEXT NOT NULL,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+    `variables` JSON NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `notification_template_key_key`(`key`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO `notification_template`
+  (`id`, `key`, `title`, `body`, `is_active`, `variables`, `created_at`, `updated_at`)
+VALUES
+  (
+    UUID(),
+    'BIRTHDAY',
+    'Feliz aniversário, {nome}! 🎉',
+    'Oi, {nome}! Hoje você completa {idade} anos. A {empresa} te deseja um dia incrível!',
+    TRUE,
+    JSON_ARRAY(
+        'nome',
+        'idade',
+        'empresa',
+        'data_aniversario'
+    ),
+    NOW(3),
+    NOW(3)
+  );
