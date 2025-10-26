04444ceb12d717bacb29c6d3b484bc947eb9d7db
Henrriky <tectutors.123@gmail.com>
2025-10-26 14:09:00 -0300

    feat(blocked-times): include rbac in blocked time routes

--- source/back-end/src/router/routes/blocked-times.routes.ts
+++ source/back-end/src/router/routes/blocked-times.routes.ts
@@ -1,10 +1,13 @@
 import { BlockedTimesController } from '@/controllers/blocked-times.controller'
+import { combinedAuthMiddleware } from '@/middlewares/auth/combined-auth.middleware'
 import { validateCreateBlockedTime } from '@/middlewares/data-validation/blocked-time/create-blocked-time.validation.middleware'
 import { validateUpdateBlockedTime } from '@/middlewares/data-validation/blocked-time/update-blocked-time.validation.middleware'
+import { UserType } from '@prisma/client'
 import { Router } from 'express'
 
 const blockedTimesRoutes = Router()
 
+blockedTimesRoutes.use(combinedAuthMiddleware([UserType.MANAGER, UserType.PROFESSIONAL], []))
 blockedTimesRoutes.get('/', BlockedTimesController.handleFindAllPaginated)
 blockedTimesRoutes.get('/:id', BlockedTimesController.handleFindById)
 blockedTimesRoutes.post('/', validateCreateBlockedTime, BlockedTimesController.handleCreate)
